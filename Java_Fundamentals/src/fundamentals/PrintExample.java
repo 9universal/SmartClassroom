a0315cfc3ed435fd0353c6ac55d0a4116ef8754b
9universal <smartclassroom111201@gmail.com>
2022-08-18 07:40:30 +0530

    Test

--- Java_Fundamentals/src/fundamentals/PrintExample.java
+++ Java_Fundamentals/src/fundamentals/PrintExample.java
@@ -5,6 +5,7 @@ public class PrintExample {
 	public static void main(String[] args) {
 		
 		System.out.println("Learning Java");
+		System.out.println("And Selenium");
 		System.out.print("With");
 		System.out.println("Smart Classroom");
 		
